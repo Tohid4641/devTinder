a88752fb7623ebcebe845a46f509fa1b72512d61
Tauhid <sktohid111@gmail.com>
2024-12-20 22:59:08 +0530

    gender schema changes

--- src/models/user.js
+++ src/models/user.js
@@ -61,7 +61,10 @@ const userSchema = new mongoose.Schema({
     },
     gender: {
         type: String,
-        enum: ['male', 'female', 'other'],
+        enum: {
+            values: ["male", "female", "other"],
+            message: '{VALUE} is not a valid gender type',
+          },
     },
     photoUrl: {
         type: String,
