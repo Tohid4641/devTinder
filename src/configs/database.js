e48ddc0f79df75db8a2b68c97f44f9aceb8923b6
Tauhid <dev.tauhid@gmail.com>
2025-01-04 17:53:21 +0530

    db uri put from env

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => await mongoose.connect(`${process.env.DATABASE_URI}/devTinder`)
+const connectDB = async () => await mongoose.connect(`mongodb+srv://devtauhid:V7DdpLNW8M8hJyTm@cluster0.dorpa.mongodb.net/devTinder`)
 
 module.exports = connectDB;
