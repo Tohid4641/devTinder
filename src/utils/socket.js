4c5091d06a2abcc3ee7335d4275ca55ee3ebca19
Tauhid <dev.tauhid@gmail.com>
2025-04-30 01:15:19 +0530

    socket.io events setup for one to one message

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -9,8 +9,23 @@ const initializeSocket = (server) => {
     io.on("connection", (socket) => {
         console.log("New client connected", socket.id);
 
-        socket.on("joinChat", () => { });
-        socket.on("sendMessage", () => { });
+        socket.on("joinChat", (data) => {
+            const { firstName, userId, targetUserId } = data;
+            const roomId = [userId, targetUserId].sort().join('_');
+
+            console.log(firstName + ": joined room :", roomId);
+            socket.join(roomId);
+
+        });
+        socket.on("sendMessage", (data) => {
+            const { firstName, userId, targetUserId, message } = data;
+            const roomId = [userId, targetUserId].sort().join('_');
+
+            io.to(roomId).emit("receivedMessage", {
+                firstName: firstName,
+                message: message,
+            });
+        });
         socket.on("disconnect", () => { });
 
 
